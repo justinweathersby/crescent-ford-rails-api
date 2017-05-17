670fc22c4f56b3870433a78c48c95cc9cf509c66
Tracy Wesley <tlynnw93@gmail.com>
2017-05-16 18:06:14 -0600

    whitelist imee's ip

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ Bundler.require(:default, :assets, Rails.env)
 
 module CrescentFord
   class Application < Rails::Application
+    config.web_console.whitelisted_ips = '10.0.0.178'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
