3e8ae3f082acd3e2565771d3031a4cbd67206b53
Tracy Lynn Wesley <NineToeNerd@users.noreply.github.com>
2017-05-18 10:44:34 -0600

    Remove changes I had added to make it work locally

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative 'boot'
 
 require 'rails/all'
-require 'net/http'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,7 +10,6 @@ Bundler.require(:default, :assets, Rails.env)
 
 module CrescentFord
   class Application < Rails::Application
-    config.web_console.whitelisted_ips = '10.0.0.178'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
